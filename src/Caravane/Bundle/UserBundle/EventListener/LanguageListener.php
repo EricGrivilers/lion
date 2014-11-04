a259c8de0a1fc2d439eeb3ce6abbc511dbfabc4f
Eric Grivilers <eric@caravanemedia.com>
2014-11-04 23:37:58 +0100

    locale

---
+++ src/Caravane/Bundle/UserBundle/EventListener/LanguageListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Caravane\Bundle\UserBundle\EventListener;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class LocaleListener
+{
+    private $session;
+
+    public function setSession(Session $session)
+    {
+        $this->session = $session;
+    }
+
+    public function setLocale(GetResponseEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            return;
+        }
+
+        $request = $event->getRequest();
+        $request->setLocale($request->getPreferredLanguage(array('fe', 'en')));
+
+    }
+}
