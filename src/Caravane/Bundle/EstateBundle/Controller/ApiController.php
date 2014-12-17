83dc1570c7447c86ac24f9e185a964a2f3cf8dbc
Eric Grivilers <eric@caravanemedia.com>
2014-12-17 13:32:31 +0100

    no cat back

--- src/Caravane/Bundle/EstateBundle/Controller/ApiController.php
+++ src/Caravane/Bundle/EstateBundle/Controller/ApiController.php
@@ -443,19 +443,14 @@ class ApiController extends RestController
             unset($datas['sort']);
         }
 
-
+        
         if($datas['category']!='') {
-            if(preg_match(",",$datas['category'])) {
-                $d=explode(",",$datas['category']);
-                $datas['category']=array_filter($d, function($k) {
-                    return $k>0;
-                });
-            }
-            else {
-                unset($datas['category']);
-            }
-        }
-        else {
+            $d=explode(",",$datas['category']);
+            $datas['category']=array_filter($d, function($k) {
+                return $k>0;
+            });
+
+        }else {
             unset($datas['category']);
         }
 
